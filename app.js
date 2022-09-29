a6e3e65c1b1f17adeeceaf921adfb0853067eb92
Alexander Chosen okon <chosenokonalexander@gmail.com>
2022-09-29 11:56:21 +0000

    I connected my app to mongoDB atlas online

---
+++ app.js
@@ -0,0 +1,28 @@
+// require both express and dotenv
+const express = require ("express")
+const { connectToMongoDb } = require("./db")
+require("dotenv").config()
+
+
+
+// port is already defined in the .env file 
+const PORT = process.env.PORT
+
+// define express and assign to a const variable
+const app = express()
+
+// connect to mongoDB instance
+connectToMongoDb()
+
+// express in-built body parser
+app.use(express.json())
+
+// define our home route
+app.get("/", (req, res)=>{
+    res.send("Welcome Home!")
+})
+
+// listen to the server and check if running
+app.listen(PORT, ()=>{
+    console.log(`Server started on PORT: http://localhost:${PORT}`)
+})
